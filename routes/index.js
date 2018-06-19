3817a7979f7cd4beee5cde4568c0f60ec0f37e16
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-06-19 13:30:18 -0400

    added project page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+class img
+{
+  constructor(path, topCaption, bottomCaption, active)
+  {
+    this.path = path;
+    this.topCaption = topCaption;
+    this.bottomCaption = bottomCaption;
+    this.active = active;
+  }
+}
+
+let images = [];
+
+images.push(new img("/images/projects/clarifyMockUp.gif", "Clarify Mockup", "Developed for HackNYU 2018", "active"));
+images.push(new img("/images/projects/kitchengif480speedup.gif", "Kitchen Prototype", "Developed for Intro to Games Development", null));
+images.push(new img("/images/projects/youup-ggjam.gif", "You Up?", "Developed for Global Game Jam 2018", null));
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { images: images });
 });
 
 module.exports = router;
