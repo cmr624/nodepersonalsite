d9676a31141a369ddbf00bb92f75f114c1a8c642
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-02-17 14:17:26 -0500

    finished bug fixes for added project content

--- routes/projects.js
+++ routes/projects.js
@@ -4,13 +4,13 @@ let projectsJSON = require("./json/projectsIndex.json")
 let projectsinfoJSON = require("./json/projects.json")
 
 
-let projects = [[projectsJSON[0], projectsJSON[1]], [[projectsJSON[2], projectsJSON[3]]]];
+let projects = [[projectsJSON[0], projectsJSON[1]], [projectsJSON[2], projectsJSON[3]]];
 
 let projectDescription = "While NYU's Computer Science program has exposed me to C, Java, Python and more, my experience in a variety of APIs through developing web apps and other projects makes me well-equipped as a developer to jump from framework to framework and deliver a quality product."
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('projects/index', { active: "projects", title: 'Projects', description:projectDescription, projects: projects});
+  res.render('games/index', { active: "projects", title: 'Projects', description:projectDescription, games: projects});
 });
 
 router.get("/clarify", function(req, res, next)
@@ -25,7 +25,7 @@ router.get("/nodepersonalsite", function(req, res, next)
 
 router.get("/assetpackages", function(req, res, next)
 {
-  res.render("templates/item.ejs", {active: "projects", css: "/stylesheets/specialclarify.css", item: projectsinfoJSON[2], links: projectsinfoJSON[2].links, credits:" Developed by Elliot Winch and Carlos-Michael Rodriguez", images: projectsinfoJSON[2].images});
+  res.render("templates/item.ejs", {active: "projects", css: "/stylesheets/gameinfo.css", item: projectsinfoJSON[2], links: projectsinfoJSON[2].links, credits:"Developed by Elliot Winch and Carlos-Michael Rodriguez", images: projectsinfoJSON[2].images});
 });
 
 module.exports = router;
